3402c0028ad199d6ec071c3381efd74c94b5dfa0
lsw04521 <lsw04521@naver.com>
2022-01-10 17:32:43 +0900

    22-01-10

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+  function left() {
+      $('#here_box li:last').prependTo('#here_box');
+      $('#here_box').css('margin-left', -1140);
+      $('#here_box').stop().animate({ marginLeft: 0 }, 400);
+  }
+
+  function right() {
+      $('#here_box').stop().animate({ marginLeft: -1140 }, 400,function () {
+          $('#here_box li:first').appendTo('#here_box');
+          $('#here_box').css({ marginLeft: 0 });
+      });
+  }
+  
+  function slide() {
+      $('#here_box').stop().animate({ marginLeft: -1140 },400, function () {
+          $('#here_box li:first').appendTo('#here_box');
+          $('#here_box').css({ marginLeft: 0 });
+      });
+  }
+  
+  $('.left').click(function(){
+      left();
+  });
+  
+  $('.right').click(function(){
+      right();
+  });
+});
