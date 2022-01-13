e593cd61df5e6aa8c95427a128bfd297e91e0b64
lsw04521 <lsw04521@naver.com>
2022-01-13 17:34:03 +0900

    22-01-13

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(function(){
       $('#here_box li:last').prependTo('#here_box');
       $('#here_box').css('margin-left', -1140);
       $('#here_box').stop().animate({ marginLeft: 0 }, 400);
-    } else {document.body.clientWidth >= 768
+    } else {
         $('#here_box li:last').prependTo('#here_box');
         $('#here_box').css('margin-left', -420);
         $('#here_box').stop().animate({ marginLeft: 0 }, 400);}
@@ -22,7 +22,7 @@ $(function(){
           $('#here_box li:first').appendTo('#here_box');
           $('#here_box').css({ marginLeft: 0 });
       });
-    } else {document.body.clientWidth >= 767
+    } else {
         $('#here_box').stop().animate({ marginLeft: -420 }, 400,function () {
             $('#here_box li:first').appendTo('#here_box');
             $('#here_box').css({ marginLeft: 0 });});}
