329fef928c57ef78e8401ec1d09a2e8d82ec69ee
lsw04521 <lsw04521@naver.com>
2022-01-13 17:30:57 +0900

    22-01-13

--- js/main.js
+++ js/main.js
@@ -6,25 +6,28 @@ $(function(){
 
 $(function(){
   function left() {
+    if ( document.body.clientWidth >= 1140) {
       $('#here_box li:last').prependTo('#here_box');
       $('#here_box').css('margin-left', -1140);
       $('#here_box').stop().animate({ marginLeft: 0 }, 400);
+    } else {document.body.clientWidth >= 768
+        $('#here_box li:last').prependTo('#here_box');
+        $('#here_box').css('margin-left', -420);
+        $('#here_box').stop().animate({ marginLeft: 0 }, 400);}
   }
 
   function right() {
+    if ( document.body.clientWidth >= 1140) {
       $('#here_box').stop().animate({ marginLeft: -1140 }, 400,function () {
           $('#here_box li:first').appendTo('#here_box');
           $('#here_box').css({ marginLeft: 0 });
       });
+    } else {document.body.clientWidth >= 767
+        $('#here_box').stop().animate({ marginLeft: -420 }, 400,function () {
+            $('#here_box li:first').appendTo('#here_box');
+            $('#here_box').css({ marginLeft: 0 });});}
   }
-  
-  function slide() {
-      $('#here_box').stop().animate({ marginLeft: -1140 },400, function () {
-          $('#here_box li:first').appendTo('#here_box');
-          $('#here_box').css({ marginLeft: 0 });
-      });
-  }
-  
+
   $('.left').click(function(){
       left();
   });
@@ -32,4 +35,6 @@ $(function(){
   $('.right').click(function(){
       right();
   });
+
+  
 });
